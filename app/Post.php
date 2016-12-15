a3960ec92ca68abbd5709b726e59725946b636d3
Dan Constantinescu <dan_constantinescu@outlook.com>
2016-12-15 23:14:04 +0200

    Comments system added

--- app/Post.php
+++ app/Post.php
@@ -13,5 +13,9 @@ class Post extends Model
     public function tags(){
         return $this->belongsToMany('App\Tag');
     }
+
+    public function comments(){
+        return $this->hasMany('App\Comment');
+    }
 }
 
--- resources/views/blog/single.blade.php
+++ resources/views/blog/single.blade.php
@@ -1,6 +1,6 @@
 @extends('main')
-
-@section('title', "| $post->title")
+<?php $titleTag = htmlspecialchars($post->title); ?>
+@section('title', "| $titleTag")
 
 @section('content')
 
@@ -11,6 +11,43 @@
             <hr>
             <p> Posted in: {{ $post->category->name }}</p>
 
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            @foreach($post->comments as $comment)
+                <div class="comment">
+                    <p><strong>Name:</strong> {{ $comment->name }}</p>
+                    <p><strong>Comment:</strong> </br>{{ $comment->comment }}</p><br>
+                </div>
+            @endforeach
+        </div>
+    </div>
+
+    <div class="row">
+        <div id="comment-form" class="col-md-8 col-md-offset-2">
+            {{ Form::open(['route'=>['comments.store',$post->id],'method'=>'POST']) }}
+
+                <div class="row">
+                    <div class="col-md-6">
+                        {{ Form::label('name','Name:') }}
+                        {{ Form::text('name',null,['class'=>'form-control']) }}
+                    </div>
+
+                    <div class="col-md-6">
+                        {{ Form::label('email','Email:') }}
+                        {{ Form::text('email',null,['class'=>'form-control']) }}
+                    </div>
+
+                    <div class="col-md-12">
+                        {{ Form::label('comment','Comment:') }}
+                        {{ Form::textarea('comment',null,['class'=>'form-control']) }}
+                        {{ Form::submit('Add Comment',['class'=>'btn btn-success btn-block']) }}
+                    </div>
+                </div>
+
+            {{ Form::close() }}
         </div>
     </div>
 
